482f4f77b630c051494ccc9706d8df98f4eafb43
Pako2425 <patryk.kocinski340@gmail.com>
2021-08-09 18:38:46 +0200

    deleted unnecessary files

--- src/rtl/top.v
+++ src/rtl/top.v
@@ -42,8 +42,7 @@ module top(
     wire vga_vsync, vga_vblnk, vga_hsync, vga_hblnk;
     
     //background_module
-    wire [11:0] rgb_out;
-    wire vga_hsync2, vga_vsync2;
+
 
     clk_gen u_clk_gen (
         .clk100MHz(clk100MHz),
@@ -68,24 +67,56 @@ module top(
         .vga_hblnk(vga_hblnk),
         .clk(clk65MHz)    
     );
+
+    //draw_car
+    wire [10:0] car_hcount_out, car_vcount_out;
+    wire car_hsync_out, car_hblnk_out, car_vsync_out, car_vblnk_out;
+    wire [16:0] pixel_addr;
+    wire [11:0] car_rgb_out;
+
+    draw_car u_draw_car(
+        .clk(clk65MHz),
+        .reset(rst),
+        .car_hcount_in(vga_hcount),
+        .car_hsync_in(vga_hsync),
+        .car_hblnk_in(vga_hblnk),
+        .car_vcount_in(vga_vcount),
+        .car_vsync_in(vga_vsync),
+        .car_vblnk_in(vga_vblnk),
+        .car_rgb_in(12'hf_f_f),
+        .car_xpos(256),
+        .car_ypos(400),
+        .car_mov(1),        
+        .car_hcount_out(car_hcount_out),
+        .car_hsync_out(car_hsync_out),
+        .car_hblnk_out(car_hblnk_out),
+        .car_vcount_out(car_vcount_out),
+        .car_vsync_out(car_vsync_out),
+        .car_vblnk_out(car_vblnk_out),
+        .car_rgb_out(car_rgb_out)
+    );
     
+
+    assign vs = car_vsync_out;
+    assign hs = car_hsync_out;
+    assign {r,g,b} = car_rgb_out;  
+
+/*
     draw_background draw_backgroud(
-        .hcount_in(vga_hcount),
-        .vcount_in(vga_vcount),
-        .hsync_in(vga_hsync),
-        .vsync_in(vga_vsync),
-        .hblnk_in(vga_hblnk),
-        .vblnk_in(vga_vblnk),
+        .hcount_in(),
+        .vcount_in(),
+        .hsync_in(),
+        .vsync_in(),
+        .hblnk_in(),
+        .vblnk_in(),
         
-        .hsync_out(vga_hsync2),
-        .vsync_out(vga_vsync2),
-        .rgb_out(rgb_out),
+        .hsync_out(),
+        .vsync_out(),
+        .rgb_out(),
         
         .clk(clk65MHz),
-        .rst(rst_ext)
+        .rst()
     );
+*/ 
 
-    assign vs = vga_vsync2;
-    assign hs = vga_hsync2;
-    assign {r,g,b} = {rgb_out[11:8], rgb_out[7:4], rgb_out[3:0]}; 
 endmodule
